2a4589296be7e82d254cc26e44ad90dc8325db3a
khadetou <khadetou96@gmail.com>
2021-08-22 13:11:46 +0000

    filter and display by category

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -52,7 +52,13 @@ export const createProducts = asyncHandler(async (req, res) => {
 //@Desc Get All products
 //@Route Get/api/products
 export const getAllProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find();
+  //filter by product
+  let categories = {};
+  if (req.query.categories) {
+    categories = { category: req.query.categories.split(",") };
+  }
+
+  const products = await Product.find(categories).populate("category");
   if (!products) {
     res.status(404);
     throw new Error("The list of products is empty");
@@ -66,11 +72,107 @@ export const getAllProducts = asyncHandler(async (req, res) => {
 //@access public
 
 export const getProductById = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id).populate("category");
 
   //Check if product is there
   if (product) {
     res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+//@desc FETCH Is featured products
+//@route Get/api/products/featured/:count
+//@access public
+
+export const getFeaturedProduct = asyncHandler(async (req, res) => {
+  const paramVal = Number(req.params.count);
+  const count = paramVal ? paramVal : 0;
+  const products = await Product.find({ isFeatured: true }).limit(count);
+
+  //Check if product is there
+  if (products) {
+    res.send(products);
+  } else {
+    res.status(404);
+    throw new Error("Products not found");
+  }
+});
+//@desc FETCH ONE  PRODUCT count
+//@route Get/api/products/count
+//@access public
+
+export const getProductCount = asyncHandler(async (req, res) => {
+  const productCount = await Product.countDocuments((count) => count);
+
+  //Check if product is there
+  if (productCount) {
+    res.send({ productCount: productCount });
+  } else {
+    res.status(404);
+    throw new Error("ProductCount not found");
+  }
+});
+
+//@desc   update product
+//@route put/api/products/:id
+//@access private, admin
+
+export const updateProduct = asyncHandler(async (req, res) => {
+  //CRATE AND UPDATE SECTION
+  const categori = await Category.findById(req.body.category);
+  if (!categori) {
+    res.status(400).send({ msg: "Invalid Category" });
+  } else {
+    const {
+      name,
+      image,
+      brand,
+      description,
+      richDescription,
+      category,
+      reviews,
+      rating,
+      numReviews,
+      price,
+      countInStock,
+      isFeatured,
+    } = req.body;
+    let product = await Product.findById(req.params.id);
+
+    if (product) {
+      (product.name = name || product.name),
+        (product.image = image || product.image),
+        (product.brand = brand || product.brand),
+        (product.category = category || product.category),
+        (product.description = description || product.description),
+        (product.richDescription = richDescription || product.richDescription),
+        (product.rating = rating || product.rating),
+        (product.numReviews = numReviews || product.numReviews),
+        (product.isFeatured = isFeatured || product.isFeatured),
+        (product.price = price || product.price),
+        (product.countInStock = countInStock || product.countInStock);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  }
+});
+
+//@desc delete product
+//@route delete/api/products/:id
+//@access Private/Admin
+export const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed" });
   } else {
     res.status(404);
     throw new Error("Product not found");
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,10 +3,22 @@ import {
   createProducts,
   getAllProducts,
   getProductById,
+  getProductCount,
+  getFeaturedProduct,
+  updateProduct,
+  deleteProduct,
 } from "../controller/productController.js";
 
 const router = express.Router();
 
-router.get("/products", getAllProducts).post("/products", createProducts);
-router.get("/products/:id", getProductById);
+router
+  .get("/products", getAllProducts)
+  .get("/products/count", getProductCount)
+  .post("/products", createProducts);
+
+router
+  .get("/products/:id", getProductById)
+  .get("/products/featured/:count", getFeaturedProduct)
+  .put("/products/:id", updateProduct)
+  .delete("/products/:id", deleteProduct);
 module.exports = router;
