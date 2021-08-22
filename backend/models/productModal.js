499af7eabd245ce4b1fe767c523007f0e3d48028
khadetou <khadetou96@gmail.com>
2021-08-22 10:04:33 +0000

    product schema

--- backend/models/productModal.js
+++ backend/models/productModal.js
@@ -1,7 +1,28 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const productSchema = mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     name: {
       type: String,
       required: true,
@@ -10,10 +31,56 @@ const productSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    brand: {
+      type: String,
+      default: "",
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    richDescription: {
+      type: String,
+      default: "",
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
     countInStock: {
       type: Number,
       required: true,
       default: 0,
+      min: 0,
+      max: 255,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
     },
   },
   {
